5865feb9145f11923ce019a6da992bff1e8ad055
rsyi <robert@ryi.me>
2019-08-20 10:42:39 -0700

    Revise version in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ copyright = '2018-2019, the authors'
 author = 'Robert Yi, Will Frost'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
